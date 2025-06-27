1f1c1a2b5ac373a062002e6ea270dc59510ee3b7
raviraj-bhalerao-iaea-org <r.bhalerao@iaea.org>
2025-06-27 11:38:41 +0200

    Delete Code/00.Connect.ps1

--- Code/00.Connect.ps1
+++
@@ -1,8 +0,0 @@
-# Import-Module Az.Accounts
-$username = "r.bhalerao@iaea.org"
-$password = ConvertTo-SecureString "ShreeRamMar2020" -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential($username, $password)
-$tenateId = "a2f21493-a4d1-4b7f-ad07-819c824f5c4a"
-Connect-AzAccount -Credential $cred #-Tenant $tenateId
-# [AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.FullName -match "Azure|Az" } | Select FullName, Location
-$ExecutionContext.SessionState.LanguageMode
